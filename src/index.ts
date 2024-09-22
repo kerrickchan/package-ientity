655a9568ddd6da29306f9f9176035dcee9ea402e
Eric C <oec88p@gmail.com>
2024-09-22 14:09:49 +0800

    remove not testable repository

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,3 @@ export * from './entity.interface';
 export * from './pagination.interface';
 export * from './repository.interface';
 export * from './memory.repository';
-export * from './sql.repository';
-export * from './nosql.repository';
